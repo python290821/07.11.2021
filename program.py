479150475b4d31c40180afe3df7fc175ceb646c5
python290821 <90146835+python290821@users.noreply.github.com>
2021-11-07 20:32:17 +0200

    Update program.py

---
+++ program.py
@@ -0,0 +1,15 @@
+#rexy = { name: 'Rexi', height: 1.2, breed:' wolf' }
+#koko = { name: 'koko', breed:' pincher' }
+
+class Dog:
+    pass
+
+rexy = Dog()
+print(rexy)
+brownie = Dog()
+print(brownie)
+print(type(rexy))
+_st = 'hello'
+print('_st is', type(_st))
+x = 1
+print('x is ',type(x))
