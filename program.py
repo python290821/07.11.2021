458ce7a476f4c5ae6d1fff4a50e08027d0087d14
python290821 <90146835+python290821@users.noreply.github.com>
2021-11-07 18:49:28 +0200

    Update program.py

---
+++ program.py
@@ -0,0 +1,21 @@
+
+# example:
+# print( {i:i for i in range(10) if i > 0} )
+
+# 1
+# create the following dictionary:
+# { 1: 1, 2: 4, 3: 9, 4: 16 ... 10: 100}
+
+# 2
+# ['I', 'Love', 'Python', 'Today']
+# create the following dictionary:
+# { 'I': 1, 'Love': 4, 'Python': 5 ...}
+
+# 2.1
+# {'jack': 38, 'john': 33 'michael': 48, 'guido': 57 }
+# create the following dictionary:
+# { 'jack': 38 ... only for items which the value is even}
+# 2.2
+# { 'jack': 'below_40', 'john':'below_40', 'michael':'above_eq_40' ...}
+
+
